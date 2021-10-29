eff6510ced8a29d3188915692c1547c14ba7854c
김영곤 <jeff.y.kim@onestorecorp.com>
2021-10-29 12:52:27 +0900

    daily

--- src/com/yg/codingtest/algorithm/DpOptimalChanges.java
+++ src/com/yg/codingtest/algorithm/DpOptimalChanges.java
@@ -1,4 +1,34 @@
 package com.yg.codingtest.algorithm;
+import java.util.*;
 
 public class DpOptimalChanges {
+
+    public static void main(String ... v) {
+//        Scanner scanner = new Scanner(System.in);
+
+//        int n = scanner.nextInt();
+//        int k = scanner.nextInt();
+        int n = 5;
+        int k = 135;
+
+        int coin[] = {5,10,50,100,500};
+//        for (int i = 0; i < n; i++) {
+//            coin[i] = scanner.nextInt();
+//        }
+
+        int d[] = new int[k + 1];
+        for (int i = 1; i <= k; i++) {
+            d[i] = -1;
+            for (int j = 0; j < n; j++) {
+                if (coin[j] <= i) {
+                    if (d[i - coin[j]] < 0) continue;
+                    if (d[i] < 0) d[i] = d[i - coin[j]] + 1;
+                    else if (d[i - coin[j]] + 1 < d[i]) d[i] = d[i - coin[j]] + 1;
+                }
+            }
+        }
+        System.out.println(d[k-5]);
+        System.out.println(d[k]);
+
+    }
 }
